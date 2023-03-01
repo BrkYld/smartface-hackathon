032e7b079ded8fec6b107a50e5b28c7b8a7e136b
burak.yildirim <burak_yildirim_official@hotmail.com>
2023-03-01 13:22:20 +0300

    devs

--- scripts/pages/pgHome.ts
+++ scripts/pages/pgHome.ts
@@ -1,6 +1,7 @@
 import PgHomeDesign from 'generated/pages/pgHome';
 import { withDismissAndBackButton } from '@smartface/mixins';
 import { Router, Route } from '@smartface/router';
+import HeaderBarItem from '@smartface/native/ui/headerbaritem';
 
 export default class PgHome extends withDismissAndBackButton(PgHomeDesign) {
   constructor(private router?: Router, private route?: Route) {
@@ -14,7 +15,15 @@ export default class PgHome extends withDismissAndBackButton(PgHomeDesign) {
   onShow() {
     super.onShow();
     //this.initBackButton(this.router); //Addes a back button to the page headerbar.
-    this.headerBar.title ='1'
+    this.headerBar.title ='Ana Sayfa'
+    var letftItem = new HeaderBarItem();
+    var rightItem = new HeaderBarItem();
+    letftItem.image = 'images://drawer.png'
+    rightItem.image = 'images://filter.png';
+    this.headerBar.setLeftItem(letftItem);
+    this.headerBar.setItems([
+        rightItem
+    ])
   }
 
   /**
