f111e4b5f18f5822892dd3acaa03eb51599c503d
burak.yildirim <burak_yildirim_official@hotmail.com>
2023-03-01 11:10:26 +0300

    route

---
+++ scripts/router/components/HomeStack.ts
@@ -0,0 +1,7 @@
+import $$HomeStack from 'generated/router/components/HomeStack';
+
+export default class HomeStack extends $$HomeStack {
+  constructor(basePath: string, name: string, routeIndex: number) {
+    super(basePath);
+  }
+}
